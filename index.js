e90f3f604372d6ad3084fb0f8672095598441a6a
TiaTay <chantay@protonmail.com>
2026-03-10 18:08:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// const greeting = document.getElementById("greeting");
+// greeting.style.textAlign = "center";
+// greeting.style.marginTop = "50px";
+// const username = window.prompt("What is your name?", "Chantay");
+// greeting.innerHTML = "Welcome to my website, " + Chantay + "!";
+
+
+
